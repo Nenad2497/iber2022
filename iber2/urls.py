20e5645f5d554132e52a32e5756b40948f07be4c
Nenad Crnoseljanski <45356940+Nenad2497@users.noreply.github.com>
2022-03-30 23:18:32 +0200

    Update urls.py

--- iber2/urls.py
+++ iber2/urls.py
@@ -29,5 +29,5 @@ urlpatterns = [
     path('', include('product.urls')),
     path('', include('customermsg.urls')),    
     #re_path(r'^media/(?P<path>.*)$', serve ,{'document_root' :settings.MEDIA_ROOT}),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.MEDIA_ROOT)
 
